6c076988d04dc66cdcc31a1afb1d505ea580c004
Regis M <regis.melgaco@gmail.com>
2018-03-14 10:33:17 -0300

    add to GitHub

--- projeto_redacao/settings.py
+++ projeto_redacao/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['projeto-redacao.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['projeto-redacao.herokuapp.com', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition

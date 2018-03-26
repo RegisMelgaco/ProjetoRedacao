12fb5dd9834f1c127ccf83b8da9e0b77a4a2db86
Regis M <regis.melgaco@gmail.com>
2018-03-26 10:04:02 -0300

    algumas funçoes que faltavam nos users

--- Usuarios/forms.py
+++ Usuarios/forms.py
@@ -34,6 +34,20 @@ class RegisterForm(forms.ModelForm):
 		return user
 
 class StudentRegisterForm(RegisterForm):
+	password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+	password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+	class Meta:
+		model = User
+		fields = ('email',)
+
+	def clean_password2(self):
+		password1 = self.cleaned_data.get("password1")
+		password2 = self.cleaned_data.get("password2")
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError("Passwords don't match")
+		return password2
+
 	def save(self, commit=True):
 		user = super(RegisterForm, self).save(commit=False)
 		user.set_password(self.cleaned_data["password1"])
@@ -43,6 +57,20 @@ class StudentRegisterForm(RegisterForm):
 		return user
 
 class TeacherRegisterForm(RegisterForm):
+	password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+	password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+	class Meta:
+		model = User
+		fields = ('email',)
+
+	def clean_password2(self):
+		password1 = self.cleaned_data.get("password1")
+		password2 = self.cleaned_data.get("password2")
+		if password1 and password2 and password1 != password2:
+			raise forms.ValidationError("Passwords don't match")
+		return password2
+		
 	def save(self, commit=True):
 		user = super(RegisterForm, self).save(commit=False)
 		user.set_password(self.cleaned_data["password1"])

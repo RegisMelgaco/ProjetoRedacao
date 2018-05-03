7579e982dea5b9850a675f022131a388605b4ba5
Régis <regis.melgaco@gmail.com>
2018-05-03 08:37:31 -0300

    Iss15 pronta

--- Redacao/admin.py
+++ Redacao/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .models import Redacao
+from .models import *
 
 class RedacaoAdmin(admin.ModelAdmin):
 	list_display = ('data_pedido', 'corrigida')
 	list_filter = ('corrigida',)
 
 	fieldsets = (
-		(None, {'fields': ('corrigida', 'data_pedido')}),
+		(None, {'fields': ('corrigida', 'data_pedido', 'proposta')}),
 		('Notas', {'fields': ('nota_competencia_1', 'nota_competencia_2', 'nota_competencia_3', 'nota_competencia_4', 'nota_competencia_5', 'nota')}),
 		('Imagens', {'fields': ('redacao', 'correcao')})
 	)
@@ -16,3 +16,4 @@ class RedacaoAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Redacao, RedacaoAdmin)
+admin.site.register(Proposta)
--- Redacao/models.py
+++ Redacao/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 class Redacao(models.Model):
 	redacao            = models.ImageField(upload_to='Redacoes/%Y/%m/%d/', null=True)
-	proposta           = models.IntegerField(null=False)
+	proposta           = models.ForeignKey('Proposta', on_delete=models.SET_NULL, null=True)
 	correcao           = models.ImageField(upload_to='Correcoes/%Y/%m/%d/', null=True)
 	data_pedido        = models.DateTimeField(null=True, default=datetime.now)
 	corrigida          = models.BooleanField(default=False, null=False)
@@ -13,3 +13,14 @@ class Redacao(models.Model):
 	nota_competencia_4 = models.IntegerField(default=0, null=True)
 	nota_competencia_5 = models.IntegerField(default=0, null=True)
 	nota               = models.IntegerField(default=0, null=True)
+
+	def __str__(self):
+		return str(self.data_pedido)
+
+class Proposta(models.Model):
+	titulo             = models.CharField(max_length=110, null=False)
+	numero             = models.IntegerField(null=False)
+	imagem             = models.ImageField(upload_to='Propostas/%Y/%m/', null=False)
+
+	def __str__(self):
+		return self.titulo
